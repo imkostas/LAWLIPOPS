96333d98d20d16e1dcfa83a8a10515eae19c9697
Chris LaVoy <clavoy@asu.edu>
2016-05-19 14:55:43 -0400

    Pull from databse and put into table

--- server.go
+++ server.go
@@ -46,29 +46,32 @@ func challengesHandler(w http.ResponseWriter, r *http.Request) {
 	display(w, "challenges", p)
 }
 
-func checkError(w http.ResponseWriter, err error) {
+func checkError(w http.ResponseWriter, err error, msg string) {
 	if err != nil {
+		log.Println(msg)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	// Open Database connection
-	// db, err := sql.Open("mysql", "root:root@/tcp(52.20.186.36)/test?tls=skip-verify$autocommit=true")
+	// db, err := sql.Open("mysql", "root:root@/tcp(52.20.186.36:80)/test?tls=skip-verify$autocommit=true")
+	// db, err := sql.Open("mysql", "root:root@/tcp(52.20.186.36)/test")
 	db, err := sql.Open("mysql", "root:root@/test")
-	checkError(w, err)
+	checkError(w, err, "Can't open db connection")
 
 	defer db.Close()
 	err = db.Ping()
-	checkError(w, err)
+	checkError(w, err, "Ping Error")
 
 	p := page{Title: "Upload File", Body: "", Files: nil, Message: ""}
 
-	rows, err := db.Query("SELECT * FROM data")
-	checkError(w, err)
+	varname := "lab"
+	rows, err := db.Query("SELECT * FROM data WHERE path LIKE '%" + varname + "%'")
+	checkError(w, err, "SELECT error")
 
 	columns, err := rows.Columns()
-	checkError(w, err)
+	checkError(w, err, "Error getting columns")
 
 	values := make([]sql.RawBytes, len(columns))
 
@@ -79,7 +82,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	for rows.Next() {
 		err = rows.Scan(scanArgs...)
-		checkError(w, err)
+		checkError(w, err, "")
 
 		// var value string
 		// for i, col := range values {
@@ -107,7 +110,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	case "POST":
 		// parse the multipart form in the request
 		err := r.ParseMultipartForm(100000)
-		checkError(w, err)
+		checkError(w, err, "")
 
 		// get a ref to the parsed multipart form
 		m := r.MultipartForm
@@ -118,7 +121,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 			// for each fileheader, get a handle to the actual file
 			file, err := files[i].Open()
 			defer file.Close()
-			checkError(w, err)
+			checkError(w, err, "")
 
 			// create destination file making sure the path is writeable.
 			dst, err := os.Create("files/" + files[i].Filename)
@@ -126,10 +129,10 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 			// 	fmt.Fprintf(w, "Unable to create the file for writing. Check your write access privilege")
 			// 	return
 			// }
-			checkError(w, err)
+			checkError(w, err, "")
 
 			defer dst.Close()
-			checkError(w, err)
+			checkError(w, err, "")
 
 			// copy the uploaded file to the destination file
 			if _, err := io.Copy(dst, file); err != nil {
@@ -139,15 +142,16 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 			// Insert into DB
 			stmtIns, err := db.Prepare("INSERT INTO `data` (`id`, `doc`, `flag`) VALUES (NULL, ?, ?);")
-			checkError(w, err)
+			checkError(w, err, "")
 			defer stmtIns.Close()
 
 			_, err = stmtIns.Exec("files/"+files[i].Filename, 0)
-			checkError(w, err)
+			checkError(w, err, "")
 		}
 
 		// display success message.
-		display(w, "upload", "Upload successful!")
+		p.Message = "Upload successful"
+		display(w, "upload", p)
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
